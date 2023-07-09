d22eb5a2e7b981d6e52c477e8eb1026f685f28d1
adviro22 <ajosueramos@gmail.com>
2023-07-09 08:46:20 -0500

    Subida de Archivos

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+// Obtener el directorio actual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Configurar el directorio de archivos estáticos
+app.use(express.static(path.join(__dirname, 'src')));
+
+// Ruta para la página principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'HTML', 'plantilla1.html'));
+});
+
+app.listen(port, () => {
+  console.log(`El servidor está escuchando en el puerto ${port}`);
+});
