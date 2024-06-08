6e500da57befb9b7bf53f925de568296351e6181
adviro22 <ajosueramos@gmail.com>
2024-06-07 21:42:49 -0500

    Plantilla 2 añadida

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src', 'HTML', 'plantilla1.html'));
 });
 
+app.get('/doc2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src', 'HTML', 'plantilla2.html'));
+});
+
 app.listen(port, () => {
   console.log(`El servidor está escuchando en el puerto ${port}`);
 });
